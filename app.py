a2bed40c994a1c2e070a9128b72731e47ca3092a
ohdnf <jupyohong7@gmail.com>
2020-01-05 01:21:27 +0900

    Authorization w/ JWT

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, current_app
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
+import bcrypt, jwt
 
 
 class CustomJSONEncoder(JSONEncoder):
@@ -122,10 +123,42 @@ def create_app(test_config = None):
     @app.route('/sign-up', methods=['POST'])
     def sign_up():
         new_user = request.json
+        new_user['password'] = bcrypt.hashpw(
+                new_user['password'].encode('UTF-8'),
+                bycrypt.gensalt()
+        )
         new_user_id = insert_user(new_user)
-        new_user = get_user(new_user_id)
+        new_user_info = get_user(new_user_id)
 
-        return jsonify(new_user)
+        return jsonify(new_user_info)
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        credential = request.json
+        email = credential['email']
+        password = credential['password']
+
+        row = database.execute(text("""
+            SELECT
+                id,
+                hashed_password
+            FROM users
+            WHERE email = :email
+        """), {'email': email}.fetchone()
+
+        if row and bcrypt.checkpw(password.encode('UTF-8'), row['hashed_password'].encode('UTF-8')):
+            user_id = row['id']
+            payload = {
+                'user_id': user_id,
+                'exp': datetime.utcnow() + timedelta(seconds = 60 * 60 * 24)
+            }
+            token = jwt.encode(payload, app.config['JWT_SECRET_KEY'], 'HS256')
+
+            return jsonify({
+                'access_token': token.decode('UTF-8')
+            })
+        else:
+            return '', 401
     
     @app.route('/tweet', methods=['POST'])
     def tweet():
